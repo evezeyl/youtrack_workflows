d07fbdaa83d14cce34c3ea79c1961dfd7d89ce22
evezeyl <evezeyl@gmail.com>
2025-05-25 14:06:55 +0200

    preparing modifications to default workflow

---
+++ js/advanced-state-machine/README.js
@@ -0,0 +1,6 @@
+/*
+Script Adapted from https://github.com/JetBrains/youtrack-workflows/js/advanced-state-machine
+NB: README.md in workflow packages will be supported soon.
+
+Advanced State Machine.
+*/
